ecd3b0ee4120fe31f2448c945a6fcd719253e564
Sara Boyer <sara.jane.boyer@gmail.com>
2020-03-31 17:42:31 -0600

    products.js created with cards

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Header } from "semantic-ui-react";
+import Products from "./Products";
 
 
 const Home = () => {
   return (
     <>
       <Header as="h1" textAlign="center">
-        Home
+        DevPoint Labs Store
+        <Products />
       </Header>
     </>
   );
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 namespace :api do
   resources :products
+    resources :purchase_products
 end
 
 namespace :api do
   resources :purchase_records
+    resources :purchase_products
 end
 
 end
