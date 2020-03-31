24d53de8d0cf2eed2561a9cf3c071c5fba33a4f9
Sara Boyer <sara.jane.boyer@gmail.com>
2020-03-31 15:07:57 -0600

    Product Controller Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  Product.Create(
+    title: Faker::Appliance.equipment,
+    description: Faker::Movies::HarryPotter.quote,
+    price: Faker::Number.decimal(l_digits: 2),
+    has_size: true,
+    sizes: {small:1,medium:1,large:1},
+    category: Faker::Company.industry,
+    main_image: '',
+    alt_image: '',
+  )
+end
+
+puts "5 Products Seeded"
