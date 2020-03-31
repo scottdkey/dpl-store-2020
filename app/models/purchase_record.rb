b9917c54937938e043a5d00be26f2f4c6062a8a5
Fabian Vazquez <Fabianxvazquez@gmail.com>
2020-03-31 14:25:39 -0600

    added migration and fixed has many through in Products

--- app/models/purchase_record.rb
+++ app/models/purchase_record.rb
@@ -1,3 +1,4 @@
-class PurchaseRecord < ApplicationRecord
-  has_many: purchase_products
+class PurchaseRecords < ApplicationRecord
+  has_many :purchase_products
+  has_many :products, through: :purchase_products
 end
