4d1a3006be3f88aa0d613dceb48692194f0e9f9e
Brianna Bell <bbell9563@gmail.com>
2020-03-31 14:28:17 -0600

    deleted register

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Fragment, } from 'react';
 import NavBar from './components/NavBar';
 import Login from './components/Login';
-import Register from './components/Register';
 import { Switch, Route, } from 'react-router-dom';
 import { Container, } from "semantic-ui-react";
 import FetchAdmin from './components/FetchAdmin';
@@ -19,7 +18,6 @@ const App = () => (
           <ProtectedRoute exact path="/" component={Home} />
           <ProtectedRoute exact path="/adminpanel" component={AdminPanel} />
           <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
         </Switch>
       </Container>
     </FetchAdmin>
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -46,13 +46,7 @@ class Navbar extends React.Component {
               active={location.pathname === "/login"}
             />
           </Link>
-          <Link to="/register">
-            <Menu.Item
-              id="register"
-              name="register"
-              active={location.pathname === "/register"}
-            />
-          </Link>
+
         </Menu.Menu>
         </>
       );
