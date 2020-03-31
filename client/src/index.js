c23d9fb4c9bf77236233076bf354255d96f989e8
Scott Key <scottdkey@gmail.com>
2020-03-30 20:47:34 -0600

    trying to get back to how things were

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter, } from 'react-router-dom';
-import { AuthProvider, } from "./providers/AuthProvider";
-import 'semantic-ui-css/semantic.min.css';
-import { initMiddleware, } from 'devise-axios';
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./providers/AuthProvider";
+import "semantic-ui-css/semantic.min.css";
+import { initMiddleware } from "devise-axios";
 
 initMiddleware();
-
-
 
 ReactDOM.render(
   <AuthProvider>
@@ -18,7 +16,7 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </AuthProvider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 serviceWorker.unregister();
