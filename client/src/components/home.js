24a87722a5e293e1b0ac56462f786f99bab9f918
Fabian Vazquez <Fabianxvazquez@gmail.com>
2020-04-01 15:16:31 -0600

    added links and created pages to display products

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Header } from "semantic-ui-react";
-import Products from "./Products";
-import AdminPanelForm from "./AdminPanelForm";
+import {Link} from 'react-router-dom'
+
 
 
 const Home = () => {
@@ -10,15 +10,19 @@ const Home = () => {
     {/* <Navbar /> */}
       <Header as="h2" textAlign="center">Categories</Header>
       <p>
-      // link to t-shirts category
-      // link to hoodies category
-      // link to hats category
-      // link to stickers category
+      {/* link to t-shirts category */}
+      <Link to="/api/products/tshirts">Tshirts</Link>
+      {/* link to hoodies category */}
+      <Link to="/api/products/hoodies">Hoodies</Link>
+      {/* link to hats category */}
+      <Link to="/api/products/hats">Hats</Link>
+      {/* link to stickers category */}
+      <Link to="/api/products/stickers">Stickers</Link>
       </p>
       <Header as="h2" textAlign="center">Featured</Header>
       // featured items
       {/* <Footer /> */}
-      //Link_to viewProduct Page, same link as viewing a product  
+      {/* Link_to viewProduct Page, same link as viewing a product   */}
     </>
   );
 };
