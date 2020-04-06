4be6d70eaf9fc6a7b842e8425fdd1f6e3ad9b1e7
Sara Boyer <sara.jane.boyer@gmail.com>
2020-04-06 12:23:25 -0600

    pulling master

--- client/src/components/Tshirts.js
+++ client/src/components/Tshirts.js
@@ -3,27 +3,7 @@ import { Card, } from 'semantic-ui-react';
 import Products from './Products';
 
 const Tshirts = () => {
-  // we have a const called tshirts which is all products with category = tshirts
-  // const products = [Product.All]
-  // const tshirts = []
-
-  // def findTshirts = () => {
-  //   return products.map( product () => (
-  //     newShirt = product
-  //     if product.category == "tshirts"
-  //       tshirts << newShirt
-  //     end
-  //   )
-  // }
-
-  // componentDidMount() {
-  //   // TODO: Make GET request with axios
-  //   // TODO: Update state
-  // }
-
-  // renderTshirts = () => {
-    
-  // };
+ 
 
   return (
     <><h1>T-Shirts</h1>
