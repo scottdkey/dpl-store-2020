eb968ec1896f8d2d9cd078f6539fa638c6ba0c34
Brianna Bell <bbell9563@gmail.com>
2020-04-07 12:18:05 -0600

    cart is stubbed out and working

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,29 +1,77 @@
 import React from 'react'
-import { getAllCartItems, deleteItemFromCart } from '../modules/CartFunctions'
+import { getAllCartItems, deleteItemFromCart, putItemInCart } from '../modules/CartFunctions'
+import { Button, Segment } from 'semantic-ui-react'
+
+// var product1 = { title: 'newProduct1' }
+// var product2 = { title: 'newProduct2' }
+// var product3 = { title: 'newProduct3' }
+// putItemInCart(product1, 's', 1)
+// putItemInCart(product2, 's', 1)
+// putItemInCart(product3, 's', 1)
 
 
 class Cart extends React.Component {
+  state = {
+    cart: []
+  }
 
+  deleteCartItem = (id) => {
+    deleteItemFromCart(id)
+    this.putItemsInCart()
+  }
+  componentDidMount() {
+    this.putItemsInCart()
+  }
 
+  putItemsInCart() {
+    let cart = getAllCartItems()
+    this.setState({ cart: cart })
+  }
 
   renderCartItems = () => {
-    let cart = getAllCartItems()
-    if (cart.length !== 0) {
-      cart.map(item => {
-        return (<div>
-          {item.object.title}
-        </div>)
-      })
+    const { cart } = this.state
+    if (cart.length > 0) {
+      return (
+        <div>
+          {cart.map(item => {
+            return (
+              <div key={`cartItem-${item.id}`}>
+                <div>
+                  {item.object.title}
+                  <div>
+                    <Button onClick={() => this.deleteCartItem(item.id)}>Remove Item</Button>
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+          <div>
+            <Button>Checkout</Button>
+          </div>
+        </div>
+      )
     }
-    else{return(<div>No items in cart</div>)}
+    else {
+      return (
+        <div>
+          No items in cart
+          <div>
+            <Button disabled>Checkout</Button>
+          </div>
+        </div>)
+    }
   }
 
   render() {
+    const { cart } = this.state
     return (
-      <div>
-        <h1>Cart</h1>
+      <>
+        <div style={{ border: '1px solid black' }}>
+          <Button >Continue Shopping</Button>
+          <h1 >My Cart</h1>
+        </div>
         {this.renderCartItems()}
-      </div>
+      </>
     )
   }
 }
