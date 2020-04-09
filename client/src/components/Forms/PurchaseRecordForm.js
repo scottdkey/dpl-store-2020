f70b6850918fd7f297f4a89117ba449c74f16fe5
Brianna Bell <bbell9563@gmail.com>
2020-04-08 21:41:49 -0600

    made cancel button in purchase_record work

--- client/src/components/Forms/PurchaseRecordForm.js
+++ client/src/components/Forms/PurchaseRecordForm.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Form, Button } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
 class PurchaseRecordForm extends React.Component {
 
@@ -43,57 +44,61 @@ class PurchaseRecordForm extends React.Component {
               onChange={this.props.handleChange}
             />
             <Form.Group widths='equal'>
-            <Form.Input
-              label="Address Line 1"
-              name="address_one"
-              required
-              placeholder="Address Line 1"
-              value={address_one}
-              fluid
-              onChange={this.props.handleChange}
-            />
-            <Form.Input
-              label="Address Line 2"
-              name="address_two"
-              placeholder="Address Line 2"
-              fluid
-              value={address_two}
-              onChange={this.props.handleChange}
-            />
+              <Form.Input
+                label="Address Line 1"
+                name="address_one"
+                required
+                placeholder="Address Line 1"
+                value={address_one}
+                fluid
+                onChange={this.props.handleChange}
+              />
+              <Form.Input
+                label="Address Line 2"
+                name="address_two"
+                placeholder="Address Line 2"
+                fluid
+                value={address_two}
+                onChange={this.props.handleChange}
+              />
             </Form.Group>
             <Form.Group widths='equal'>
-            <Form.Input
-              label="City"
-              name="city"
-              required
-              placeholder="City"
-              value={city}
-              onChange={this.props.handleChange}
-            />
-            <Form.Input
-              label="State"
-              name="state"
-              placeholder="State"
-              required
-              value={state}
-              onChange={this.props.handleChange}
-            />
-            <Form.Input
-              label="Zip Code"
-              name="zip_code"
-              required
-              placeholder="Zip Code"
-              value={zip_code}
-              onChange={this.props.handleChange}
-            />
+              <Form.Input
+                label="City"
+                name="city"
+                required
+                placeholder="City"
+                value={city}
+                onChange={this.props.handleChange}
+              />
+              <Form.Input
+                label="State"
+                name="state"
+                placeholder="State"
+                required
+                value={state}
+                onChange={this.props.handleChange}
+              />
+              <Form.Input
+                label="Zip Code"
+                name="zip_code"
+                required
+                placeholder="Zip Code"
+                value={zip_code}
+                onChange={this.props.handleChange}
+              />
             </Form.Group>
           </Form>
           <div style={style.buttonHolder}>
-          <Button style={style.cancelBtn}>Cancel</Button>
-           <Button style={style.submitBtn} onClick={this.props.handleSubmit}>Place My Order</Button>
+            <div style={{width:"45%"}}>
+            <Link to='/'><Button style={style.cancelBtn}>Cancel</Button></Link>
+            </div>
+            <div style={{width:"45%"}}>
+            <Button style={style.submitBtn} onClick={this.props.handleSubmit}>Place My Order</Button>
+            </div>
           </div>
         </div>
-      </div>
+      </div >
     )
   }
 }
@@ -102,8 +107,8 @@ const style = {
   formHolder: {
     borderRadius: '10px',
     boxShadow: '5px 5px 20px #d1d1d1',
-    marginTop:'3%',
-    marginBottom:'5%'
+    marginTop: '3%',
+    marginBottom: '5%'
   },
   formHeader: {
     backgroundColor: '#ededed',
@@ -116,22 +121,22 @@ const style = {
   form: {
     padding: '10px'
   },
-  submitBtn:{
+  submitBtn: {
     color: 'white',
     backgroundColor: '#4901DB',
     borderRadius: '30px',
-    width:'45%',
-    marginTop:'3%'
+    marginTop: '3%',
+    width:'100%'
   },
-  cancelBtn:{
+  cancelBtn: {
     color: 'red',
     borderRadius: '30px',
-    marginTop:'3%',
-    width:'45%'
+    marginTop: '3%',
+    width:'100%'
   },
-  buttonHolder:{
-    display:'flex',
-    justifyContent:'space-between'
+  buttonHolder: {
+    display: 'flex',
+    justifyContent: 'space-between'
   }
 }
 
