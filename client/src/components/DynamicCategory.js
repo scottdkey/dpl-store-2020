f67c204a61e8fb594ef805af62aa1433e41005b9
Fabian Vazquez <Fabianxvazquez@gmail.com>
2020-04-16 12:18:17 -0600

    fixed navbar and added hook folder

--- client/src/components/DynamicCategory.js
+++ client/src/components/DynamicCategory.js
@@ -1,16 +1,18 @@
-  
 import React, { useState, useEffect } from "react";
 import { Card, Image, Button } from "semantic-ui-react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import BlueHeader from "../images/BlueHeader2.svg";
 import FunctionalSearch from "./SharedComponents/FunctionalSearch";
-import Products from './Products'
+import Products from "./Products";
+import { useGetProducts } from '../hooks/useGetProducts'
 
 const DynamicCategory = ({ category_id, match, category_name, noHeader }) => {
-  const [items, setItems] = useState([]);
-  const [category, setCategory] = useState(null);
   const cat_id = category_id || match.params.category_id;
+  const [category, setCategory] = useState(cat_id);
+  const items = useGetProducts(cat_id)
+
+  // const [items, setItems] = useState([]);
   const [results, setResults] = useState([]);
   // make another useEffect to get the category
   // /categories/:category_id/
@@ -19,92 +21,71 @@ const DynamicCategory = ({ category_id, match, category_name, noHeader }) => {
   // /categories/:category_id/products/:product_id
   // call to get both of them
   const afterSearch = (results) => setResults(results);
-  const renderResults = () => results.map((result) => (
-    <div key={result.id}>
-      {result.title}
-    </div>
-  ));
-
-
-  useEffect(() => {
-    const cat_id = category_id || match.params.category_id;
-    axios
-      .get(`/api/categories/${cat_id}/products`)
-      .then(res => {
-        setItems(res.data);
-      })
-      .catch(console.log);
-  }, []);
-
+  const renderResults = () =>
+    results.map((result) => <div key={result.id}>{result.title}</div>);
 
   // gets category on initial render
   useEffect(() => {
-    const cat_id = category_id || match.params.category_id;
     axios
       .get(`/api/categories/${cat_id}`)
-      .then(res => setCategory(res.data))
+      .then((res) => setCategory(res.data))
       .catch(console.log);
-  }, []);
+  }, [cat_id]);
 
-
-  const renderItems = () =>
-  <div style={style.productContainer}>
-        { items.map((product) => (
-            <div  key={product.id}>
-              <div style={{ ...style.photoHolder }}>
-                <div style={style.crop}>
-                  <Image
-                    src={product.main_image}
-                    as={Link}
-                    to={{
-                      pathname: `/categories/${cat_id}/products/${product.id}`,
-                      state: { ...product },
-                    }}
-                  />
-                </div>
-              </div>
-              <div style={style.informationContainer}>
-                <div>
-                  <h3 style={{ margin: "5px", display: "inline" }}>
-                    {"$" + product.price}
-                  </h3>
-                  <h5 style={{ margin: "5px", display: "inline" }}>
-                    {product.title}
-                  </h5>
-                </div>
-              </div>  
+  const renderItems = () => (
+    <div style={style.productContainer}>
+      {items.map((product) => (
+        <div key={product.id}>
+          <div style={{ ...style.photoHolder }}>
+            <div style={style.crop}>
+              <Image
+                src={product.main_image}
+                as={Link}
+                to={{
+                  pathname: `/categories/${cat_id}/products/${product.id}`,
+                  state: { ...product },
+                }}
+              />
             </div>
-        ))}
-  </div>
+          </div>
+          <div style={style.informationContainer}>
+            <div>
+              <h3 style={{ margin: "5px", display: "inline" }}>
+                {"$" + product.price}
+              </h3>
+              <h5 style={{ margin: "5px", display: "inline" }}>
+                {product.title}
+              </h5>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 
-  if(noHeader){
+  if (noHeader) {
+    return <>{renderItems()}</>;
+  } else {
     return (
       <>
-        {renderItems()}
-      </>
-    )
-  } else {
-    return(
-      <>
-      <div class="image-container">
+        <div class="image-container">
           <Image src={BlueHeader} style={{ width: "100%" }} />
-          <div class= "centered">
+          <div class="centered">
             <h1 class="large-header">{category && category.name}</h1>
-            <FunctionalSearch afterSearch={afterSearch} category_id={cat_id}/>
-            </div>
-            </div>
-            {results.length > 0 && renderResults()}
-            {renderItems()}
-            <br />
-          <div align="center">
+            <FunctionalSearch afterSearch={afterSearch} category_id={cat_id} />
+          </div>
+        </div>
+        {results.length > 0 && renderResults()}
+        {renderItems()}
+        <br />
+        <div align="center">
           <button style={style.button}>See More</button>
-      </div>
-      <br />
+        </div>
+        <br />
       </>
-    )
-  };
+    );
+  }
 };
-
 
 const style = {
   crop: {
@@ -141,11 +122,11 @@ const style = {
     width: "125px",
   },
   productContainer: {
-    display: 'flex',
-    alignItems: 'stretch',
-    marginLeft: '100px',
-    flexWrap: 'wrap',
+    display: "flex",
+    alignItems: "stretch",
+    marginLeft: "100px",
+    flexWrap: "wrap",
   },
 };
 
-export default DynamicCategory; 
+export default DynamicCategory;
