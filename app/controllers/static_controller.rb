870104c95fde63da97ec3f4473c91795b6e6b96e
Scott Key <scottdkey@gmail.com>
2020-04-20 11:40:29 -0600

    heroku changes

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,6 @@
 class StaticController < Rails::ApplicationController
   layout false
-  def index    
-    render file: Rails.root.join("public", "index.html")
+  def index
+    render file: Rails.root.join('public', 'index.html')
   end
 end
