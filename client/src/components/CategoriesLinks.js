59d88ee0e956bb94e4562f583283f1f4317beae7
Fabian Vazquez <Fabianxvazquez@gmail.com>
2020-04-20 22:50:10 -0600

    removed centered from category links line 22

--- client/src/components/CategoriesLinks.js
+++ client/src/components/CategoriesLinks.js
@@ -19,7 +19,7 @@ const CategoriesLinks = () => {
   const renderLinks = () =>
     categories.map( (category) => (
       <div key={`${category.id}`}>
-        <Grid.Column centered  as={Link} to={`/categories/${category.id}/products`}>
+        <Grid.Column as={Link} to={`/categories/${category.id}/products`}>
           <Image src={category.image} alt={category.name} style={styles.image}/>
           <h4 align="center">{category.name}</h4>
         </Grid.Column>
